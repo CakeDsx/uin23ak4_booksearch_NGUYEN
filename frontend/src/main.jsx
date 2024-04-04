f4bcfd89ebe989181933838dc38d960211050d05
CakeDsx <philipvtn@gmail.com>
2024-04-04 19:42:38 +0200

    filer

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { AppProvider } from './components/pages/Context'
+import './index.scss'
+import Home from './components/pages/Home/Home'
+import About from './components/pages/About/About'
+import BookList from './components/BookList/BookList'
+import BookDetails from './components/BookDetails/BookDetails'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}>
+          <Route path = "about" element = {<About />} />
+          <Route path = "book" element = {<BookList />} />
+          <Route path = "book/:id" element = {<BookDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
 )
